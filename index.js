78f44e04aee0025dadbeb5497d4a088881111661
Asive Nzwana <aive.nzwana@marstech.co.za>
2025-05-13 15:36:59 +0200

    Setup Time Zone

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const MORNING_TIME = process.env.MORNING_TIME || '06:00'; // Default 8:00 AM
 const GREENAPI_ID_INSTANCE = process.env.GREENAPI_ID_INSTANCE;
 const GREENAPI_API_TOKEN = process.env.GREENAPI_API_TOKAEN;
 
+
+
 // Handle multiple phone numbers
 // Phone numbers can be specified in the .env file as comma-separated values
 
@@ -172,6 +174,9 @@ const [hours, minutes] = MORNING_TIME.split(':');
 cron.schedule(`${minutes} ${hours} * * *`, async () => {
   console.log(`It's ${MORNING_TIME}! Sending morning SMS...`);
   await sendMorningSMS();
+}, {
+  scheduled: true,
+  timezone: "Africa/Johannesburg"  
 });
 
 // Simple web interface
